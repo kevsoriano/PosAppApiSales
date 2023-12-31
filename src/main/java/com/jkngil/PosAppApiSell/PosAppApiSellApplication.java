d01d5fc95c949d17b6e99f0b4efbccc6788fe44b
Kevin Gil Soriano <kevingilsoriano@Kevins-Mac-mini.local>
2023-12-31 22:12:27 +0800

    first commit

---
+++ src/main/java/com/jkngil/PosAppApiSell/PosAppApiSellApplication.java
@@ -0,0 +1,13 @@
+package com.jkngil.PosAppApiSell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAppApiSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAppApiSellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkngil/PosAppApiSell/PosAppApiSellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkngil.PosAppApiSell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAppApiSellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
